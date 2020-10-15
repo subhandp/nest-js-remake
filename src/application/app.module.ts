127e8de0738c9b35aa28d64bb6c1bbf396bd713d
Aditia Pratama <aditiapratamagg@gmail.com>
2020-10-15 17:49:15 +0700

    Register domain as entity

--- src/application/app.module.ts
+++ src/application/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LoginModule } from './use_cases/user/login/login.module';
-import { configService } from 'src/infrastructure/config/config'
+import { databaseConfigService } from 'src/infrastructure/config/db.config'
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     LoginModule,
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+    TypeOrmModule.forRoot(databaseConfigService.getTypeOrmConfig())
   ],
 })
 export class AppModule {}
