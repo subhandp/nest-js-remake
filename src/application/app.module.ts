828153cd5b1c44d96889cc428b5f1450dd3a4256
Aditia Pratama <aditiapratamagg@gmail.com>
2020-10-15 17:36:18 +0700

    TypeORM Integration

--- src/application/app.module.ts
+++ src/application/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LoginModule } from './use_cases/user/login/login.module';
+import { configService } from 'src/infrastructure/config/config'
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [LoginModule],
+  imports: [
+    LoginModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+  ],
 })
 export class AppModule {}
